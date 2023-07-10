2ccf5735c3b6a3f5dd12b902c553f5d364ecbd62
“rafalhalasa” <rafal.halasa@gmail.com>
2023-07-10 14:50:40 +0200

    change view base on document

--- app/src/main/java/pl/simcodic/mybestmovie/presentation/main/MainScreen.kt
+++ app/src/main/java/pl/simcodic/mybestmovie/presentation/main/MainScreen.kt
@@ -1,16 +1,17 @@
 package pl.simcodic.mybestmovie.presentation.main
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -23,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -68,31 +70,34 @@ fun MainScreenContainer(
 
 @Composable
 fun MoviesList(moviesData: List<NowPlayingMovieViewData>, modifier: Modifier = Modifier) {
-    LazyColumn(
-        modifier = modifier
-            .fillMaxSize()
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 160.dp), modifier = modifier
     ) {
         items(moviesData.size) {
             val movie = moviesData[it]
             Card(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
+                    .height(250.dp)
                     .padding(10.dp),
                 elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
             ) {
-                Row {
+                Box {
                     AsyncImage(
                         model = "https://image.tmdb.org/t/p/w500${movie.posterPath}",
                         contentDescription = null,
-                        modifier = Modifier.height(200.dp)
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
                     )
-                    Column(modifier = Modifier.padding(5.dp)) {
-                        Text(text = "${movie.voteAverage} / ${movie.voteCount}")
-                        Spacer(modifier = Modifier.height(20.dp))
-                        Text(text = movie.title, style = MaterialTheme.typography.titleMedium)
-                        Spacer(modifier = Modifier.height(10.dp))
-                        Text(text = movie.releaseDate, style = MaterialTheme.typography.bodyMedium)
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f)),
+                        contentAlignment = Alignment.TopEnd
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.baseline_star_border),
+                            contentDescription = null
+                        )
                     }
                 }
             }
