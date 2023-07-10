be721636773755146b7af19dc82dda9ce65ade26
“rafalhalasa” <rafal.halasa@gmail.com>
2023-07-10 09:11:40 +0200

    add usecase to view model

--- app/src/main/java/pl/simcodic/mybestmovie/domain/movie/GetMoviesUseCase.kt
+++ app/src/main/java/pl/simcodic/mybestmovie/domain/movie/GetMoviesUseCase.kt
@@ -1,8 +1,9 @@
 package pl.simcodic.mybestmovie.domain.movie
 
 import pl.simcodic.mybestmovie.domain.base.UseCase
+import javax.inject.Inject
 
-class GetMoviesUseCase : UseCase {
+class GetMoviesUseCase @Inject constructor() : UseCase {
     override fun run() {
 
     }
--- app/src/main/java/pl/simcodic/mybestmovie/presentation/main/MainViewModel.kt
+++ app/src/main/java/pl/simcodic/mybestmovie/presentation/main/MainViewModel.kt
@@ -1,9 +1,18 @@
 package pl.simcodic.mybestmovie.presentation.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import pl.simcodic.mybestmovie.domain.movie.GetMoviesUseCase
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(getMoviesUseCase: GetMoviesUseCase) : ViewModel() {
+
+    init {
+        viewModelScope.launch {
+            getMoviesUseCase()
+        }
+    }
 }
