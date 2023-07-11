31d19c56de83c2c5821d8526e0055f660877aea5
“rafalhalasa” <rafal.halasa@gmail.com>
2023-07-11 08:42:30 +0200

    add navigation

--- app/src/main/java/pl/simcodic/mybestmovie/presentation/main/MainActivity.kt
+++ app/src/main/java/pl/simcodic/mybestmovie/presentation/main/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import pl.simcodic.mybestmovie.presentation.theme.AppTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import pl.simcodic.mybestmovie.presentation.main.Destinations.*
+import pl.simcodic.mybestmovie.presentation.theme.AppTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -16,8 +20,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                MainScreen(viewModel)
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = HOME.value) {
+                    composable(HOME.value) {
+                        MainScreen(viewModel)
+                    }
+                    composable(DETAILS.value){
+                        MainScreen(viewModel)
+                    }
+                }
             }
         }
     }
+}
+
+enum class Destinations(val value: String) {
+    HOME("home"), DETAILS("details_screen")
 }
